bf4792eb33b876b8086fb2e1dfb92831f54e42e3
feroult <feroult@gmail.com>
2018-02-20 21:55:00 -0300

    mercado

---
+++ extensions/exchanges/mercado/update-products.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+const ccxt = require ('ccxt')
+const c = require('../../../conf')
+const n = require('numbro')
+
+const mercado = new ccxt.mercado({
+  'apiKey': c.mercado.key,
+  'secret': c.mercado.secret,
+})
+
+
+var hardcodedOptions = {
+    BTC: {
+        min_size: '0.002',
+        max_size: '30',
+        step: 0.001
+    },
+    LTC: {
+        min_size: '0.08',
+        max_size: '5000.0',
+        step: 0.001
+    },
+    BCH: {
+        min_size: '0.02',
+        max_size: '2000',
+        step: 0.001
+    }
+};
+
+mercado.fetch_markets()
+  .then(result =>   {
+    var products = []
+
+    for (var key in result) {
+        if (!result.hasOwnProperty(key)) {
+            continue;
+        }
+        var product = result[key]
+        products.push({
+            asset: product.base,
+            currency: product.quote,
+            min_size: hardcodedOptions[product.base].min_size,
+            max_size: hardcodedOptions[product.base].max_size,
+            increment: n(hardcodedOptions[product.base].step).format('0.000000000000000000'),
+            label: product.symbol
+        })
+    }
+    var target = require('path').resolve(__dirname, 'products.json')
+    require('fs').writeFileSync(target, JSON.stringify(products, null, 2))
+    console.log('wrote', target)
+    process.exit()
+  })
+  .catch(function (error) {
+    console.error('An error occurred', error)
+    process.exit(1)
+  })
